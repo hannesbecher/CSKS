702771c3e900d1eeeebdd5df650be41c92215bb1
Hannes Becher <hannesbecher@gmx.de>
2021-11-15 19:56:37 +0000

    Update makeGenomes.py

--- code/makeGenomes.py
+++ code/makeGenomes.py
@@ -10,7 +10,7 @@ print("########################################\nGENOME MAKING SCRIPT")
 rng = np.random.default_rng()
 
 gc=0.5
-gs = int(sys.argv[1])
+gs = int(sys.argv[1])   # size of one chromosome (10 are simulated)!
 ploy = int(sys.argv[2])
 theta = float(sys.argv[3])
 tdir = sys.argv[4]
