ed401fc800f2d8a913e8e915f0ee83edfad57e99
Hannes Becher <hannesbecher@gmx.de>
2021-11-15 19:55:12 +0000

    Update makeReads.py

--- code/makeReads.py
+++ code/makeReads.py
@@ -1,6 +1,6 @@
 # make sequencing reads from genomes file (generated using makeGenomes.py)
 
-# USAGE python makeReads.py tdir
+# USAGE: python makeReads.py tdir
 import sys
 import numpy as np
 
@@ -9,18 +9,21 @@ print("########################################\nREAD MAKING SCRIPT")
 tdir = sys.argv[1]
 
 print("Reading genomes...")
+# the file has two lines: header and data
 with open(tdir + "/genomes.fa", "r") as infile:
     hd, dat = infile.readlines()
 
 cov = 30 # seq coverage (per haploid genome)
 rlen = 150 # read length
 
-pl = int(hd[:-1].split("_")[1][1:])    
-hgs = int(hd[:-1].split("_")[2][1:])    
-tlen = int(hd[:-1].split("_")[-1][1:])
+pl = int(hd[:-1].split("_")[1][1:])    # get ploidy from header line
+hgs = int(hd[:-1].split("_")[2][1:])   # get haploid genome size from header line
+tlen = int(hd[:-1].split("_")[-1][1:]) # get total genome size from header line
 
+# compute number of reads to generate
 nreads = hgs * cov * pl // rlen
 
+# sample read locations
 rstarts = np.random.choice(range(tlen-rlen), nreads, replace=True)
 
 print("Writing reads to %s/reads.fa..." % tdir)
