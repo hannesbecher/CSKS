03894f0f1da3987695644c2baafd3ebaf076cc63
Hannes Becher <hbecher@ed.ac.uk>
2026-03-18 10:49:49 +0000

    produce log file

--- 01pipeline.sh
+++ 01pipeline.sh
@@ -1,7 +1,3 @@
-# generate k-mer spectrum according to genome and population parameters
-echo "########################################"
-echo "COALESCENT SIMULATION FOR K-MER SPRECTRA"
-
 # activate 'conda' environment (run 00setUpEnv.sh to set up)
 #micromamba activate CSKS
 
@@ -19,10 +15,6 @@ Options:
   -h, --help     Show this help message and exit
 EOF
 }
-
-# make a temp dir 
-td=$(mktemp -d)
-
 
 # simulation parameters
 gs=1000000 # Size in nt of each chromosome. There are 10 chromosomes, so the overall haploid genome size is 'gs'*10!
@@ -82,6 +74,19 @@ while [ "$#" -gt 0 ]; do
     esac
 done
 
+hist_file="G${gs}P${ploy}T${theta}.hist"
+hist_no0_file="${hist_file}.no0"
+log_file="G${gs}P${ploy}T${theta}.log"
+
+exec > >(tee "$log_file") 2>&1
+
+# generate k-mer spectrum according to genome and population parameters
+echo "########################################"
+echo "COALESCENT SIMULATION FOR K-MER SPRECTRA"
+
+# make a temp dir
+td=$(mktemp -d)
+
 # run msprime (this produces the genomes)
 python code/makeGenomes.py "$gs" "$ploy" "$theta" "$td"
 
@@ -97,14 +102,15 @@ echo ""
 echo "########################################"
 echo "MAKING SPECTRUM"
 # run kmc_tools to make spectrum
-kmc_tools transform  $td/db21 histogram  G$gs'P'$ploy'T'$theta.hist -cx5000
+kmc_tools transform  $td/db21 histogram  "$hist_file" -cx5000
 echo "removing zero lines"
-awk '{ if( $2 != 0 ){ print $0 } }' G$gs'P'$ploy'T'$theta.hist > G$gs'P'$ploy'T'$theta.hist.no0 && rm G$gs'P'$ploy'T'$theta.hist
+awk '{ if( $2 != 0 ){ print $0 } }' "$hist_file" > "$hist_no0_file" && rm "$hist_file"
 echo "Done."
 echo ""
 
 echo "########################################"
 echo "Removing temp files..."
 rm -rf "$td" # comment out for debug!
-echo "Histogram written to G"$gs"P"$ploy"T"$theta".hist.no0"
+echo "Histogram written to $hist_no0_file"
+echo "Log written to $log_file"
 echo "PIPELINE DONE."
